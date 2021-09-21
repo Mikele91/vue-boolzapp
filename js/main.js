cff3108108de11b05064471328e7dc6ce5af8170
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-21 13:52:04 +0200

    aggiunto lente in input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const app = new Vue({
+    el:"#root",
+    data:{
+        
+    }
+})
+
+
+
+
 // Milestone 1
 // ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
 // dall’interlocutore (bianco) assegnando due classi CSS diverse
